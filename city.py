1c20bb5b9a31eb71a4c7ae44254d981f93240772
Alexander <24574245+SobertKaos@users.noreply.github.com>
2018-03-20 17:23:54 +0100

    Make city base runnable
    Makes city a runnable base version of the model. Adds static_variable to dispatchmodel for investment variables

--- city.py
+++ city.py
@@ -8,12 +8,10 @@ import pdb
 import pandas as pd
 import numpy as np
 import friendlysam as fs
+import partlib as pl
 import matplotlib.pyplot as plt
 from models import DispatchModel
 from copy import deepcopy
-from partlib import (Resources, HeatPump, Import, Export, LinearCHP,
-                     LinearSlowCHP, Boiler, PipeLoss, Accumulator)
-
 # logger = logging.getLogger(__name__)
 # logging.basicConfig(level=logging.DEBUG)
 
@@ -21,53 +19,48 @@ print("using pandas version ", pd.__version__)
 print("using python version", sys.version)
 
 
-class HeatNetworkModel():
+class CityModel():
 
     def __init__(self):
         print('Initializing model')
-
+        self.m = None
+        self.solution = None
         self._DEFAULT_PARAMETERS = {
             'time_unit': pd.Timedelta('1h'),  # Time unit
-            'step': pd.Timedelta('48h'),  # Time span to lock in each step
-            'horizon': pd.Timedelta('72h'),  # Planning horizon
-            'prices': {
-                Resources.heating_oil: 52.88,  # 500 SEK/MWh (LHV)
-                Resources.bio_oil: 52.88,  # 500 SEK/MWh (LHV)
-                Resources.natural_gas: 29.61,  # 280 SEK/MWh (LHV)
-                Resources.wood_chips: 21.15,  # 200 SEK/MWh (LHV)
-                Resources.wood_pellets: 320,  # 320 SEK/MWh (LHV)
-                Resources.waste: 0,  # Assumed lower than others, really != 0
+            't_start': pd.Timestamp('2015-01-01'),
+            't_end': pd.Timestamp('2015-01-02'), 
+            'prices': { # €/MWh (LHV)
+                pl.Resources.natural_gas: 7777,
+                pl.Resources.power: 7777,
+                pl.Resources.heat: 7777,
+                pl.Resources.heating_oil: 7777,
+                pl.Resources.bio_oil: 7777,
+                pl.Resources.wood_chips: 7777,
+                pl.Resources.wood_pellets: 7777,
+                pl.Resources.waste: 7777,
                 }
         }
 
-
-        self.dynamicParts = set()
-
-    def RunModel(self, time_unit, natural_gas_price):
+    def RunModel(self):
         print('Running model')
-        seed = 2
-        parameters = self.get_parameters(
-            time_unit=pd.Timedelta(str(time_unit)+"h"),
-            step=pd.Timedelta('24h'))
-        self.t0 = self.get_heat_history(parameters['time_unit']).index[0]
-        t_end = self.get_heat_history(parameters['time_unit']).index[-1]
-        # MSE : because FS cannot handle end of files
-        self.t_end = t_end.replace(day=t_end.day-4) # INFO Was 3
-        # self.t0 = pd.Timestamp('2013-01-01')
-        # self.t_end = pd.Timestamp('2014-01-01')
-        print("Make model")
-        self.m = self.make_model(parameters, natural_gas_price, seed=seed)
-        self.m.time=self.t0 
-        self.m.time_end= self.t_end
-        self.m.step = parameters['step']
-        
-        self.m.solver = fs.get_solver()
+        seed = None
+        parameters = self.get_parameters()     
+        t_start = parameters['t_start']
+        t_end = parameters['t_end']
+
+        print("Making model")
+        self.m = self.make_model(parameters, seed=seed)
+        #self.m.time= t_start
+        #self.m.time_end= t_end
+        #self.m.solver = fs.get_solver()
         _start_time = pd.Timestamp.now()
         print("Beginning simulation")
         self.m.solve()
         _end_time = pd.Timestamp.now()
         _elapsed = _end_time-_start_time
         print('Done! Simulation took {0:0.0f} seconds'.format(_elapsed.total_seconds()))
+        
+        
 
     def set_heat_history(self, p_heat_history):
         self.heat_history_file = p_heat_history
@@ -81,91 +74,53 @@ class HeatNetworkModel():
     def set_fixed_price(self, p_fixed_price):
         self.fixed_price_file = p_fixed_price
 
+    def read_csv(self, file_path, index_col = 'Time (UTC)', **kwargs):
+        read_file = pd.read_csv(file_path,
+                                encoding='utf-8',
+                                index_col= index_col,
+                                parse_dates=True,
+                                **kwargs)
+        return read_file
+
     def get_heat_history(self, time_unit):
-        heat_history = pd.read_csv(
-            self.heat_history_file,
-            # self.heat_history_raw,
-            encoding='utf-8',
-            index_col='Time (UTC)',
-            parse_dates=True)
+        heat_history = self.read_csv(self.heat_history_file)
         return heat_history.resample(time_unit).sum()
 
     def get_heat_history_industry(self, time_unit):
-        heat_history_industry = pd.read_csv(
-            'industrial_load_generated.csv',
-            encoding='utf-8',
-            index_col='Time (UTC)',
-            parse_dates=True)
+        heat_history_industry = self.read_csv('industrial_load_generated.csv')
         return heat_history_industry.resample(time_unit).sum()
 
     def get_power_demand(self, time_unit):
-        power_demand = pd.read_csv(
-            self.power_demand_file,
-            # self.power_demand_raw,
-            encoding='utf-8',
-            index_col='Time (UTC)',
-            parse_dates=True,
-            squeeze=True)
+        power_demand = self.read_csv(self.power_demand_file, squeeze=True)
         return power_demand.resample(time_unit).sum()
 
     def get_power_price(self, time_unit):
         print(self.power_price_file)
-        power_price = pd.read_csv(
-            self.power_price_file,
-            encoding='utf-8',
-            index_col='Time (UTC)',
-            parse_dates=True,
-            squeeze=True)
+        power_price = self.read_csv(self.power_price_file, squeeze=True)
         return power_price.resample(time_unit).mean()
-
-    def get_fixed_price(self, resources):
-        print(self.fixed_price_file)
-        with open(self.fixed_price_file) as f:
-            f.readline()
-            prices = dict(csv.reader(f, delimiter=','))
-        resource_dict = {}
-
-        for r in resources:
-            # Splits the string following '<Resources.'
-            key = repr(r)[11:].split(':')[0]
-            resource_dict[key] = r
-
-        fixed_prices = {}
-        for key, value in prices.items():
-            fixed_prices[resource_dict[key]] = float(value)
-
-        return fixed_prices
 
     def get_parameters(self, **kwargs):
         parameters = deepcopy(self._DEFAULT_PARAMETERS)
         parameters.update(kwargs)
         return parameters
 
-    def make_model(self, parameters, natural_gas_price, seed=None):
-        uncertain = DummyRandomizer() if seed is None else Randomizer(seed)
-        print("Get power price")
-        parameters['prices'][Resources.power] = self.get_power_price(parameters['time_unit'])
-        print("Set gas price to ", natural_gas_price)
-        parameters['prices'][Resources.natural_gas] = natural_gas_price
-        print("Get fixed prices")
-        parameters['prices'].update(self.get_fixed_price(Resources))
+    def make_model(self, parameters, seed=None):
+        if seed:
+            raise NotImplementedError('Randomizer not yet supported')
         print("building model")
-        model = DispatchModel()
-#            horizon=int(parameters['horizon'] / parameters['time_unit']),
-#            step=int(parameters['step'] / parameters['time_unit']))
+        model = DispatchModel(t_start=parameters['t_start'],
+                              t_end=parameters['t_end'],
+                              time_unit=parameters['time_unit'])
 
-        parts = self.make_parts(parameters, uncertain)
+        parts = self.make_parts(parameters)
         # No explicit distribution channels except for heat.
-        for r in Resources:
-            if r == Resources.heat:
-                pass
-            else:
+        for r in pl.Resources:
+            if r is not pl.Resources.heat:
                 cluster = fs.Cluster(resource=r, name='{} cluster'.format(r))
                 for p in parts:
-                    if type(p) is not fs.parts.FlowNetwork:
+                    if not isinstance(p, fs.parts.FlowNetwork) and (r in p.resources):
                         # FlowNetwork has no attribute ´resources´
-                        if r in p.resources:
-                            cluster.add_part(p)
+                        cluster.add_part(p)
                     else:
                         model.add_part(p)
                 cluster.cost = lambda t: 0
@@ -176,21 +131,22 @@ class HeatNetworkModel():
 
         return model
 
-    def addParts(self, part):
-        self.dynamicParts.add(part)
-
-    def make_parts(self, parameters, uncertain):
+    def make_parts(self, parameters):
         parts = set()
+        def make_tax_function(*args, **kwargs):
+            def tax_function(*args, **kwargs):
+                return 0
+            return  tax_function
 
-        heat_history = self.get_heat_history(parameters['time_unit'])
-        power_demand = self.get_power_demand(parameters['time_unit'])
-        heat_history_industry = self.get_heat_history_industry(parameters['time_unit'])
-        taxation = self.make_tax_function(parameters)
+        #heat_history = self.get_heat_history(parameters['time_unit'])
+        #power_demand = self.get_power_demand(parameters['time_unit'])
+        #heat_history_industry = self.get_heat_history_industry(parameters['time_unit'])
+        taxation = make_tax_function(parameters)
 
-        for r in Resources:
-            if r is not Resources.heat:
+        for r in pl.Resources:
+            if r is not pl.Resources.heat:
                 parts.add(
-                    Import(
+                    pl.Import(
                         resource=r,
                         price=parameters['prices'][r],
                         name='Import({})'.format(r)))
@@ -204,132 +160,128 @@ class HeatNetworkModel():
         hour = pd.Timedelta('1h') / parameters['time_unit']
         series_reader = lambda series: series.loc.__getitem__
         city = fs.Node(name='City')
-        city.consumption[Resources.heat] = series_reader(heat_history.sum(axis=1))
-        city.consumption[Resources.power] = series_reader(power_demand)
+        city.consumption[pl.Resources.heat] = lambda t: 14
+        city.consumption[pl.Resources.power] = lambda t: 13
         city.cost = lambda t: 0
         city.state_variables = lambda t: ()
         parts.add(city)
 
         Industry = fs.Node(name='Industry')
-        Industry.consumption[Resources.heat] = series_reader(heat_history_industry.sum(axis=1))
+        Industry.consumption[pl.Resources.heat] = lambda t: 12
         Industry.cost = lambda t: 0
         Industry.state_variables = lambda t: ()
         parts.add(Industry)
 
-        powerExport = Export(resource = Resources.power,
+        powerExport = pl.Export(resource = pl.Resources.power,
                              capacity=1000 / hour, # Arbitrarily chosen, assumed higher than combined production
-                             price = parameters['prices'][Resources.power]/10)
+                             price = parameters['prices'][pl.Resources.power]/10)
         parts.add(powerExport)
 
         parts.add(
-            LinearSlowCHP(
+            pl.LinearSlowCHP(
                 name='CHP A',
                 eta=0.776, # was 77.6
                 alpha=0.98,
                 Fmax=4.27 / hour,  # 449.0 / hour,  # 449 m3/hour
                 Fmin=2.33 / hour,  # 245.0 / hour,  # 245 m3/hour
                 start_steps=int(np.round(.5 * hour)),
-                fuel=Resources.natural_gas,
+                fuel=pl.Resources.natural_gas,
                 taxation=taxation))
 
         parts.add(
-            LinearSlowCHP(
+            pl.LinearSlowCHP(
                 name='CHP B',
                 eta=0.778, #was 77.8
                 alpha=0.98,
                 Fmax=4.27 / hour,  # 449.0 / hour,  # 449 m3/hour
                 Fmin=2.33 / hour,  # 245.0 / hour,  # 245 m3/hour
                 start_steps=int(np.round(.5 * hour)),
-                fuel=Resources.natural_gas,
+                fuel=pl.Resources.natural_gas,
                 taxation=taxation))
 
         parts.add(
-            Boiler(
+            pl.Boiler(
                 name='Boiler A',
                 eta=0.9,
                 Fmax=8.84 / hour,  # 930.89 / hour,  # Sm3/hour
                 # Fmin=135.52 / hour,#Sm3/hour
-                fuel=Resources.natural_gas,
+                fuel=pl.Resources.natural_gas,
                 taxation=taxation))
 
         parts.add(
-            Boiler(
+            pl.Boiler(
                 name='Boiler B',
                 eta=0.87,
                 Fmax=8.84 / hour,  # 930.89 / hour,  # Sm3/hour
                 # Fmin=135.52 / hour,#Sm3/hour
-                fuel=Resources.natural_gas,
+                fuel=pl.Resources.natural_gas,
                 taxation=taxation))
 
         parts.add(
-            Boiler(
+            pl.Boiler(
                 name='Boiler C',
                 eta=0.89,
                 Fmax=8.84 / hour,  # 930.89 / hour,  # Sm3/hour
                 # Fmin=465.44 / hour,#Sm3/hour
-                fuel=Resources.natural_gas,
+                fuel=pl.Resources.natural_gas,
                 taxation=taxation))
 
         parts.add(
-            Boiler(
+            pl.Boiler(
                 name='Boiler D',
                 eta=0.77,
                 Fmax=8.84 / hour,  # 930.89 / hour,  # Sm3/hour
                 # Fmin=465.44 / hour,#Sm3/hour
-                fuel=Resources.natural_gas,
+                fuel=pl.Resources.natural_gas,
                 taxation=taxation))
 
-        def make_tax_function(*args, **kwargs):
-            return lambda x: 0
-
         parts.add(
-            LinearSlowCHP(
+            pl.LinearSlowCHP(
                 name='Waste Incinerator',
                 start_steps=12,
-                fuel=Resources.waste,
+                fuel=pl.Resources.waste,
                 alpha=0.46,
                 eta=0.81,
                 Fmin=10 / hour,
                 Fmax=45 /hour, # Update to reflect 25 MW output max according to D4.2 p 32
                 taxation=taxation))
         
-        accumulator = Accumulator(
-                resource=Resources.heat,
+        parts.add(
+            pl.Accumulator(
+                resource=pl.Resources.heat,
                 max_flow=6/hour,
                 max_energy=68,
                 loss_factor = 0,
-                name='TES')
-
-        parts.add(accumulator)
+                name='TES'))
 
         heat_producers = {p for p in parts
-                          if Resources.heat in p.production and
+                          if ((pl.Resources.heat in p.production) or
+                              (pl.Resources.heat in p.accumulation)) and
                           not isinstance(p, fs.Cluster)}
-        production_cluster = fs.Cluster(resource=Resources.heat,
+        production_cluster = fs.Cluster(resource=pl.Resources.heat,
                                         name='production cluster')
         production_cluster.cost = lambda t: 0
 
         for p in heat_producers:
             production_cluster.add_part(p)
-        production_cluster.add_part(accumulator)
 
         parts.add(production_cluster)
 
         heat_consumers = {p for p in parts
-                          if Resources.heat in p.consumption and
+                          if pl.Resources.heat in p.consumption and
                           not isinstance(p, fs.Cluster)}
 
-        consumption_cluster = fs.Cluster(resource=Resources.heat,
+        consumption_cluster = fs.Cluster(resource=pl.Resources.heat,
                                          name='consumption cluster')
         consumption_cluster.cost = lambda t: 0
         for p in heat_consumers:
-            if not isinstance(p, Accumulator):
+            if not isinstance(p, pl.Accumulator):
                 consumption_cluster.add_part(p)
         consumption_cluster.add_part(powerExport)
         parts.add(consumption_cluster)
 
         
-        pipe = fs.FlowNetwork(Resources.heat)
+        pipe = fs.FlowNetwork(pl.Resources.heat)
         pipe.cost = lambda t: 0
         pipe.connect(production_cluster, consumption_cluster)
         parts.add(pipe)
@@ -484,5 +436,8 @@ class HeatNetworkModel():
 
 
 if __name__ == "__main__":
-    pass
-
+    print('Running city.py standalone')
+    import pdb
+    model = CityModel()
+    solution = model.RunModel()
+    pdb.set_trace()
